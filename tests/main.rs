23b1905b7f27d5e9f59a4f6c1b35e1ceddecd222
unknown <bisongbest04@gmail.com>
2026-07-14 12:17:20 +0100

    test: add integration test crate as workspace member with extern crate declarations

---
+++ tests/main.rs
@@ -0,0 +1,13 @@
+extern crate tellus_pool;
+extern crate tellus_policy;
+extern crate tellus_oracle;
+extern crate tellus_trigger;
+
+mod integration_test;
+pub use integration_test::{create_token_contract, setup_env_with_time};
+
+mod end_to_end_tests;
+mod oracle_tests;
+mod policy_tests;
+mod pool_tests;
+mod trigger_tests;
